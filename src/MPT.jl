6d0b63fa9d7e3ad6f5bc37cc5761771cfb7d77c3
cmmenzen <c.m.menzen@tudelft.nl>
2022-03-01 14:15:32 +0100

    add functions to get element/row/col from TT/TTm

--- src/MPT.jl
+++ src/MPT.jl
@@ -56,3 +56,72 @@
     Base.getindex(mpt::MPT, range::UnitRange{Int64}) = [mpt.cores[i] for i in range]
     Base.setindex!(mpt::MPT,v,i::Int) = setindex!(mpt.cores, v, i) # tt[1] = rand(1,5,3) sets the first core
 
+
+    # indexing to get an element or row/column of the reconstructed vector or matrix
+
+    function getelement(tt::MPT{3},row::Int)
+        sz = size(tt);
+        d  = ();
+        el = 1;
+        for i = 1:length(sz)
+            d = (d...,1:sz[i][1])
+        end
+        linear = LinearIndices(d);
+        index = findall(x->x==row,linear)[1];
+        for i = 1:length(sz)
+            el = el*tt[i][:,index[i],:];
+        end
+        return el[1]
+    end
+
+    function getelement(ttm::MPT{4},row::Int,col::Int)
+        sz = size(ttm);
+        d1  = ();
+        d2  = ();
+        el = 1;
+        for i = 1:length(sz)
+            d1 = (d1...,1:sz[i][1])
+            d2 = (d2...,1:sz[i][2])
+        end
+        linear1 = LinearIndices(d1);
+        index1 = findall(x->x==row,linear1)[1];
+        linear2 = LinearIndices(d2);
+        index2 = findall(x->x==col,linear2)[1];
+        for i = 1:length(sz)
+            el = el*ttm[i][:,index1[i],index2[i],:];
+        end
+        return el[1]
+    end
+
+
+    function getcol(ttm::MPT{4},col::Int)
+        sz = size(ttm);
+        d  = ();
+        for i = 1:length(sz)
+            d = (d...,1:sz[i][1])
+        end
+        linear = LinearIndices(d);
+        index = findall(x->x==col,linear)[1];
+
+        cores = Vector{Array{Float64,3}}(undef,order(ttm));
+        for i = 1:length(sz)
+            cores[i] = ttm[i][:,:,index[i],:];
+        end
+        return MPT(cores)
+    end
+
+    function getrow(ttm::MPT{4},row::Int)
+        sz = size(ttm);
+        d  = ();
+        for i = 1:length(sz)
+            d = (d...,1:sz[i][2])
+        end
+        linear = LinearIndices(d);
+        index = findall(x->x==row,linear)[1];
+
+        cores = Vector{Array{Float64,3}}(undef,order(ttm));
+        for i = 1:length(sz)
+            cores[i] = ttm[i][:,index[i],:,:];
+        end
+        return MPT(cores)
+    end
