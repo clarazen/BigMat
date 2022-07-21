2fe4abf9bf2bf7d23661d61ab8d803ea52863a29
cmmenzen <c.m.menzen@tudelft.nl>
2022-07-21 14:07:26 +0200

    add kr2mat

--- src/BigMat.jl
+++ src/BigMat.jl
@@ -8,6 +8,7 @@ using LinearAlgebra, Base, Random, SparseArrays,MATLAB
            getelement, getrow, getcol, outerprod,
            MPT_SVD, TTm_ALS, TT_ALS, eye, rkrp2tn,
            sparseblockTTm, ALS_krtt, BayALS_krtt,
+           kr2mat,
            mps2vec, mpo2mat,
            KhatriRao, transpose, roundTT, 
            krtimesttm,
